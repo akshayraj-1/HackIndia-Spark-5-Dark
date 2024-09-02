0a2a51afadf4cac21b59d9b25b9686ce2ef5d788
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 04:26:26 +0530

    Modified MainBackground.jsx

--- frontend/src/components/Backgrounds/MainBackground.jsx
+++ frontend/src/components/Backgrounds/MainBackground.jsx
@@ -3,8 +3,10 @@ import cn from "../../utils/cn.util.js";
 
 function MainBackground({ children, className }) {
     return (
-        <div className="relative size-full">
-            <div className={cn("absolute z-[2] h-full w-full", className)}>
+        <div className="relative size-full before:absolute
+        before:inset-0 before:bg-main before:bg-cover
+        before:bg-no-repeat before:bg-center before:opacity-50">
+            <div className={cn("absolute z-[2] size-full", className)}>
                 {children}
             </div>
         </div>
