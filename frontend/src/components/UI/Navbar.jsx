72b799957103e4da05e0cd47f5b1928e90fcbb19
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 04:26:26 +0530

    Modified Navbar.jsx

--- frontend/src/components/UI/Navbar.jsx
+++ frontend/src/components/UI/Navbar.jsx
@@ -3,10 +3,10 @@ import Button from "./Button.jsx";
 
 function Navbar() {
     return (
-        <nav className="fixed top-0 w-full flex justify-between items-center px-5 sm:px-8 py-4 border-b-2 border-b-secondary backdrop-blur bg-primary/50">
+        <nav className="sticky top-0 w-full flex justify-between items-center px-5 sm:px-8 py-4 border-b-2 border-b-secondary backdrop-blur bg-primary/50">
             <Link className="font-semibold text-2xl" to="/">freezone</Link>
             <div>
-                <Button variant="primary" label="Connect" size={"md"}/>
+                <Button variant="secondary" label="Join Now" size="md" className="border-accent/30"/>
             </div>
         </nav>
     );
