d06f8b7c8cf9a7b210d6db60fb6471e81d92d700
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 04:26:26 +0530

    Modified Post.jsx

--- frontend/src/components/UI/Post.jsx
+++ frontend/src/components/UI/Post.jsx
@@ -1,25 +1,30 @@
 import {motion} from "framer-motion";
 import PropTypes from "prop-types";
 import cn from "../../utils/cn.util.js";
-function Post({id, author, textContent, images, onClick, styles}) {
+
+function Post({ id, owner, timestamp, content, image, onClick, styles }) {
     return (
-        <motion.div className={cn("bg-secondary rounded-md border border-tertiary")}>
-            <div className="flex items-center justify-start">
-                <motion.img src={author?.profile} className="size-14 rounded-full"/>
-                <div>
-                    <p className="text-primaryText font-medium">{author?.name}</p>
-                    <p className="text-secondaryText font-medium">{"@ " + author?.username}</p>
-                </div>
+        <motion.div className={cn("flex flex-col gap-2 bg-secondary/50 rounded-xl px-4 py-4 border border-tertiary overflow-hidden")}>
+            <div className="flex flex-col items-start justify-start">
+                <p className="text-primaryText font-medium">{owner}</p>
+                <p className="text-secondaryText font-medium">{new Date(timestamp * 1000).toLocaleString()}</p>
             </div>
+            {
+                content && <p className="text-primaryText">{content}</p>
+            }
+            {
+                image && <img src={image} className="h-86 aspect-square object-cover"/>
+            }
         </motion.div>
     );
 }
 
 Post.propTypes = {
-    id: PropTypes.string,
-    author: PropTypes.string,
-    textContent: PropTypes.string,
-    images: PropTypes.array,
+    id: PropTypes.string.isRequired,
+    owner: PropTypes.string.isRequired,
+    timestamp: PropTypes.number.isRequired,
+    content: PropTypes.string,
+    image: PropTypes.string,
     onClick: PropTypes.func
 }
 
