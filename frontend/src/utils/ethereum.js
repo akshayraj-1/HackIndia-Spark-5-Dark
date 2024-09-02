75846a17b18e7fe38477cb495284ede23cbec197
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 02:32:29 +0530

    Modified ethereum.js

--- frontend/src/utils/ethereum.js
+++ frontend/src/utils/ethereum.js
@@ -5,7 +5,7 @@ import * as SocialMediaJson from "../../../backend/artifacts/contracts/SocialMed
 const ALCHEMY_SEPOLIA_API_URL = import.meta.env.VITE_ALCHEMY_SEPOLIA_API_URL;
 const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
 
-const getReadOnlyProvider = () => {
+const getJsonRpcProvider = () => {
     if (!ALCHEMY_SEPOLIA_API_URL) {
         console.error("ALCHEMY_SEPOLIA_API_URL is missing");
         return null;
@@ -13,13 +13,30 @@ const getReadOnlyProvider = () => {
     return new ethers.JsonRpcProvider(ALCHEMY_SEPOLIA_API_URL);
 };
 
+const getBrowserProvider = () => {
+    if (!CONTRACT_ADDRESS || !SocialMediaJson.abi) {
+        console.error("Contract address or ABI is missing");
+        return null;
+    }
+    return new ethers.BrowserProvider(window.ethereum);
+};
+
 const getReadOnlyContract = async () => {
     if (!CONTRACT_ADDRESS || !SocialMediaJson.abi) {
         console.error("Contract address or ABI is missing");
         return null;
     }
-    const provider = getReadOnlyProvider();
+    const provider = getJsonRpcProvider();
     return new ethers.Contract(CONTRACT_ADDRESS, SocialMediaJson.abi, provider);
 };
 
-export { getReadOnlyContract };
+const getBrowserContract = async () => {
+    if (!CONTRACT_ADDRESS || !SocialMediaJson.abi) {
+        console.error("Contract address or ABI is missing");
+        return null;
+    }
+    const provider = getBrowserProvider();
+    return new ethers.Contract(CONTRACT_ADDRESS, SocialMediaJson.abi, provider);
+};
+
+export { getReadOnlyContract, getBrowserContract };
