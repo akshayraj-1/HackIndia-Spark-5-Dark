060bd913ed8507478abc9006b09643d13641a3e0
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 04:26:26 +0530

    Modified Button.jsx

--- frontend/src/components/UI/Button.jsx
+++ frontend/src/components/UI/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import {motion} from "framer-motion";
 import cn from "../../utils/cn.util.js";
 
-function Button({type = "button", variant = "primary", size = "sm", label, onClick, href}) {
+function Button({type = "button", variant = "primary", size = "sm", label, onClick, href, className}) {
 
     const Element = type === "button" ? motion.button : motion.a;
 
@@ -13,15 +13,15 @@ function Button({type = "button", variant = "primary", size = "sm", label, onCli
             size === "lg" && "text-[1rem] px-5 py-3"
         ),
         primary: cn("text-primary font-medium bg-accent rounded-md"),
-        secondary: "text-primaryText font-medium bg-secondary rounded-md",
+        secondary: "text-primaryText font-medium border border-tertiary hover:bg-accent hover:text-primary bg-secondary/50 rounded-md",
         tertiary: "text-primaryText font-medium bg-transparent rounded-md border border-tertiary hover:bg-tertiary/50 transition"
     }
 
     return (
-        <Element className={cn(styles.text, styles[variant])}
+        <Element className={cn(styles.text, styles[variant], className)}
                  href={href}
                  onClick={onClick}
-                 whileTap={{ y: 1, transition: { duration: 0.1, ease: "easeInOut" } }}
+                 whileTap={{ scale:0.99, y: 1, transition: { duration: 0.05, ease: "easeInOut" } }}
         >
             {label}
         </Element>
@@ -34,7 +34,8 @@ Button.propTypes = {
     size: PropTypes.oneOf(["sm", "md", "lg"]),
     label: PropTypes.string,
     onClick: PropTypes.func,
-    href: PropTypes.string
+    href: PropTypes.string,
+    className: PropTypes.string
 }
 
 export default Button;
