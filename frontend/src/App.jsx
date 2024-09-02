a6fa5528b6580aae0fcc2b8fae6c6cd42db2dbc3
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 04:26:26 +0530

    Modified App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import {Suspense} from "react";
 import {Outlet} from "react-router-dom";
 import Navbar from "./components/UI/Navbar.jsx";
+import MainBackground from "./components/Backgrounds/MainBackground.jsx";
 
 
 function App() {
     return (
-        <>
+        <MainBackground className="overflow-hidden">
             <Navbar/>
             <Suspense>
                 <Outlet/>
             </Suspense>
-        </>
+        </MainBackground>
     );
 }
 
