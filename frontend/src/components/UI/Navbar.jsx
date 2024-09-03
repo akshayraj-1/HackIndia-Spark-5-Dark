c075744b6930023cc9f33747885f07703503b4d6
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 09:02:55 +0530

    Modified Navbar.jsx

--- frontend/src/components/UI/Navbar.jsx
+++ frontend/src/components/UI/Navbar.jsx
@@ -1,12 +1,24 @@
 import {Link} from "react-router-dom";
 import Button from "./Button.jsx";
+import {useWalletContext} from "../../contexts/useWalletContext.jsx";
+import {useEffect} from "react";
 
 function Navbar() {
+
+    const {account, connectWallet, disconnectWallet} = useWalletContext();
+
+    useEffect(() => {
+    }, [account]);
+
     return (
-        <nav className="sticky top-0 w-full flex justify-between items-center px-5 sm:px-8 py-4 border-b-2 border-b-secondary backdrop-blur bg-primary/50">
+        <nav className="sticky top-0 z-10 w-full flex justify-between items-center px-5 sm:px-8 py-4 border-b-2 border-b-tertiary backdrop-blur bg-primary/50">
             <Link className="font-semibold text-2xl" to="/">freezone</Link>
             <div>
-                <Button variant="secondary" label="Join Now" size="md" className="border-accent/30"/>
+                {
+                    account ?
+                        <Button label="Disconnect" onClick={disconnectWallet}/> :
+                        <Button label="Connect" onClick={connectWallet}/>
+                }
             </div>
         </nav>
     );
