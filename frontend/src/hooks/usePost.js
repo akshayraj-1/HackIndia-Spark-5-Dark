6cb47d2a4dff4bf5951376e6b4573520dcc9206e
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 09:59:26 +0530

    Modified usePost.js

--- frontend/src/hooks/usePost.js
+++ frontend/src/hooks/usePost.js
@@ -1,17 +1,28 @@
 import {useState} from "react";
 import PropTypes from "prop-types";
 import { getJsonRpcContract, getBrowserContract } from "../utils/ethereum.js";
+import {StorageDownloader, ThirdwebStorage} from "@thirdweb-dev/storage";
 
 function usePost() {
 
     const [loading, setLoading] = useState(false);
+    // TODO: Image Upload
+    // const storage = new ThirdwebStorage({
+    //     secretKey: import.meta.env.VITE_THIRDWEB_STORAGE_API
+    // })
+
 
     const createPost = async (content, image) => {
         try {
             const contract = await getBrowserContract();
+            let imageUri = "";
+            // if (image) {
+            //     imageUri = await storage.upload(image);
+            // }
+            console.log(imageUri);
             return await contract.createPost(content, image);
         } catch (e) {
-            console.log(e);
+            console.error(e.message);
         }
     };
 
