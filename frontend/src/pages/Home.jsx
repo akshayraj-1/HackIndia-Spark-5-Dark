375d839c826ea865768fdfa9f3d0a9bce8bf7e8d
akshayraj-1 <akshayraj4101@gmail.com>
2024-09-03 09:59:26 +0530

    Modified Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import {NavLink} from "react-router-dom";
 import usePost from "../hooks/usePost.js";
 import {useEffect, useState} from "react";
 import PostCard from "../components/UI/PostCard.jsx";
 import {useWalletContext} from "../contexts/useWalletContext.jsx";
+import Button from "../components/UI/Button.jsx";
 
 function Home() {
 
@@ -17,14 +17,20 @@ function Home() {
 
     }, []);
 
+    const handleCreatePost = () => {
+        // TODO: Create Post
+        // createPost("Sample Post 2", "https://images.unsplash.com/photo-1725067807370-a3fc3fd23000?q=80&w=2068&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+        //     .then(res => getAllPosts().then(posts => setPosts(posts)));
+    }
+
     return (
         <main className="relative flex justify-center items-start gap-4 md:gap-6 size-full py-4 px-5 sm:px-8">
 
             <div className="sticky top-20 bg-secondary/70 rounded-md border-2 border-tertiary px-4 py-2">
-                <NavLink to="/create" onClick={createPost}>Create</NavLink>
+                <Button label="Create Post" variant="primary" onClick={handleCreatePost}/>
             </div>
 
-            <div id="posts" className="relative flex flex-col gap-3 h-full min-h-full w-full sm:w-[320px] md:w-[600px]">
+            <div id="posts" className="relative flex flex-col gap-3 h-full min-h-full w-full sm:w-[320px] md:w-[600px] pb-4">
                 {
                     posts?.map((post) => {
                         console.log(post);
